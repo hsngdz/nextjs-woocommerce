a3b889040c45a388831856758055ef8af1ff4742
Hassan Goodarzi <hassan@violetgem.com>
2023-11-12 22:32:29 +0330

    use Apollo Client with configured middleware and afterware

--- lib/woocommerce/queries/cart.ts
+++ lib/woocommerce/queries/cart.ts
@@ -1,4 +1,14 @@
+import { gql } from '@apollo/client';
+
 import { cartContentFragment, customerContentFragment } from '../fragments/cart';
+
+export const getCartDocument = gql`
+  query {
+    customer {
+      sessionToken
+    }
+  }
+`;
 
 export const getCartQuery = /* GraphQL */ `
   query getCart($customerId: Int) {
